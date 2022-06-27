d68829f5522ad628eaadf6c4a02effeb48fc6693
Pat McDonald <patmcdonald@me.com>
2022-06-27 17:27:35 +0000

    declare const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-wCYhpQ0wTgdat0HVCPl512wxFjM
+const API_KEY ="wCYhpQ0wTgdat0HVCPl512wxFjM";
+const API_URL = "http://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
