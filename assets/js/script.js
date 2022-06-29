ce7883460893396cf53390d84a21a82f4c5bc4a3
Pat McDonald <patmcdonald@me.com>
2022-06-29 10:21:07 +0000

    options comma seperate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,64 @@
-const API_KEY ="wCYhpQ0wTgdat0HVCPl512wxFjM";
+const API_KEY = "wCYhpQ0wTgdat0HVCPl512wxFjM";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-async function postForm(e){
-  const form = new FormData(document.getElementById("checksform"));
+function processOptions(form) {
+  
+let optArray = [];
+
+for (let entry of form.entries()) {
+  if (entry[0] === "options") {
+    optArray.push(entry[1]);
+  }
+}
+form.delete("options");
+
+form.append("options", optArray.join());
+
+return.form;
+
+}
+
+async function postForm(e) {
+  const form = processOptions(new FormData(document.getElementById("checksform")));
 
   const response = await fetch(API_URL, {
-                        method: "POST",
-                        headers: {
-                                    "Authorization": API_KEY,
-                                 },
-                                 body:  form,
-                        });
-                       
+    method: "POST",
+    headers: {
+      "Authorization": API_KEY,
+    },
+    body: form,
+  });
+
   const data = await response.json();
-    if (response.ok) {
-  displayErrors(data);
-    }  else {
-      throw new  Error(data.error);
-    }      
+  if (response.ok) {
+    displayErrors(data);
+  } else {
+    throw new Error(data.error);
+  }
 }
 
 
 
 async function getStatus(e) {
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
+  const queryString = `${API_URL}?api_key=${API_KEY}`;
 
-    const response = await fetch(queryString);
+  const response = await fetch(queryString);
 
-    const data = await response.json();
+  const data = await response.json();
 
-      if (response.ok) {
-        displayStatus(data);
-    } else {
-      throw new Error(data.error);
-    }
+  if (response.ok) {
+    displayStatus(data);
+  } else {
+    throw new Error(data.error);
+  }
 }
 
 function displayErrors(data) {
-    
+
   let heading = `JSHint results for ${data.file}`;
 
   if (data.total_errors === 0) {
@@ -49,8 +66,8 @@ function displayErrors(data) {
   } else {
     results = `<div>tTotal Errors: <span class="error_count">${data.total_errors}</span></div>`;
     for (let error of data.error_list) {
-      results += `<div>At line <span class="line >${error.line}</span> , ` ;
-      results  += `column <span class = "column">${error.col}:</span></div>`;
+      results += `<div>At line <span class="line >${error.line}</span> , `;
+      results += `column <span class = "column">${error.col}:</span></div>`;
       results += `<div class="error">${error.error}</div>`;
     }
   }
@@ -62,13 +79,13 @@ function displayErrors(data) {
 }
 
 function displayStatus(data) {
- 
-  let heading =  "API Key Status";
+
+  let heading = "API Key Status";
   let results = `<div>Your key is valid until</div>`;
   results += `<div class = "key-status">${data.expiry}</div>`;
 
-document.getElementById("resultsModalTitle").innerText = heading;
-document.getElementById("results-content").innerHTML = results;
-resultsModal.show();
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
+  resultsModal.show();
 
 }
